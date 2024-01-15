d1846ea2cdaa213235a8baabdd77110bd454d570
Mouslih0 <maromouslih@gmail.com>
2024-01-15 15:40:29 +0100

    add Material model and Echontillon model

---
+++ src/main/java/com/example/labxpert/Model/EchontillonMaterial.java
@@ -0,0 +1,29 @@
+package com.example.labxpert.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "echnotillon_material")
+public class EchontillonMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Echontillon echontillon;
+
+    @ManyToOne
+    private Material material;
+
+    private int quantity;
+
+    private double price_total;
+}
---
+++ src/main/java/com/example/labxpert/Model/Material.java
@@ -0,0 +1,22 @@
+package com.example.labxpert.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "materials")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String libelle;
+    private int availableQuantity;
+    private double price;
+}
