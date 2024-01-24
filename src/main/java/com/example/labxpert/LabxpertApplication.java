5f853d8c39d620030ec280b620e88fed05f7075d
Mouslih0 <maromouslih@gmail.com>
2024-01-24 15:11:09 +0100

    firt commit in part 3

--- src/main/java/com/example/labxpert/LabxpertApplication.java
+++ src/main/java/com/example/labxpert/LabxpertApplication.java
@@ -3,7 +3,6 @@ package com.example.labxpert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class LabxpertApplication{
 
